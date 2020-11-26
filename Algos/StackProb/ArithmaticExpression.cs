8ee4bb4bbf5ad9a52939258cde85307febdb5f6d
Nitin Mahajan <nimaha@microsoft.com>
2020-11-26 18:41:19 +0530

    Stack - Expression Postfix / Infix and Queue implementation

---
+++ Algos/StackProb/ArithmaticExpression.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algos.StackProb
+{
+    public static class ArithmaticExpression
+    {
+        public static void Test()
+        {
+            UnitTest("5+2", 7);
+            UnitTest("5+2*2", 9);
+            UnitTest("5*2*2", 20);
+            UnitTest("5*2/2+2", 7);
+            UnitTest("50*20/20+20", 70);
+
+            UnitTest_PostFix("5 2 +", 7);
+            UnitTest_PostFix("50 20 * 20 / 20 +", 70);
+        }
+
+        private static void UnitTest(string expr, int result)
+        {
+            int expected = EvaluateInfix(expr);
+            Console.WriteLine($"{expected == result}, {expr} = {expected}");
+        }
+
+        private static bool IsOperator(char valOp)
+        {
+            return valOp == '*' || valOp == '/' || valOp == '+' || valOp == '-';
+        }
+
+        private static string ApplyOperator(string num1, Stack<string> stack, char op)
+        {
+            int p1 = int.Parse(num1);
+            int p2 = int.Parse(stack.Pop());
+            if (op == '*')
+            {
+                return $"{p1 * p2}";
+            }
+            else if (op == '/')
+            {
+                return $"{p2 / p1}";
+            }
+            else if (op == '+')
+            {
+                return $"{p1 + p2}";
+            }
+            else if (op == '-')
+            {
+                return $"{p1 - p2}";
+            }
+            throw new Exception("Invalid op");
+        }
+
+        public static int EvaluateInfix(string expr)
+        {
+            var stack = new Stack<string>();
+            string prevNumber = "";
+            char lastOperator = '\0';
+            for (int i = 0; i < expr.Length; i++)
+            {
+
+                if (char.IsNumber(expr[i]))
+                {
+                    prevNumber += expr[i];
+                }
+
+                if (!char.IsNumber(expr[i]) || i == expr.Length - 1)
+                {
+                    if (lastOperator == '*' || lastOperator == '/')
+                    {
+                        prevNumber = ApplyOperator(prevNumber, stack, lastOperator);
+                    }
+
+                    stack.Push(prevNumber);
+                    prevNumber = string.Empty;
+                    lastOperator = expr[i];
+
+                    if (lastOperator == '+' || lastOperator == '-')
+                    {
+                        stack.Push(expr[i].ToString());
+                    }
+                }
+            }
+
+            while (stack.Count != 0)
+            {
+                if (stack.Peek() == "+" || stack.Peek() == "-")
+                {
+                    prevNumber = ApplyOperator(prevNumber, stack, stack.Pop()[0]);
+                }
+                else
+                {
+                    prevNumber = stack.Pop();
+                }
+            }
+            return int.Parse(prevNumber);
+        }
+
+
+        private static void UnitTest_PostFix(string expr, int result)
+        {
+            int expected = EvaluatePostfix(expr);
+            Console.WriteLine($"{expected == result}, {expr} = {expected}");
+        }
+
+        public static int EvaluatePostfix(string expr)
+        {
+            var arrExpr = expr.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var stack = new Stack<string>();
+            for (int i = 0; i < arrExpr.Length; i++)
+            {
+                if (IsOperator(arrExpr[i][0]))
+                {
+                    var prevNumber = ApplyOperator(stack.Pop(), stack, arrExpr[i][0]);
+                    stack.Push(prevNumber);
+                }
+                else
+                {
+                    stack.Push(arrExpr[i]);
+                }                
+            }
+
+            return int.Parse(stack.Pop());
+        }
+    }
+}
---
+++ Algos/StackProb/QueueImpl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algos.StackProb
+{
+    public class QueueImpl<T>
+    {
+        public static void Test()
+        {
+            var queue = new QueueImpl<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+
+            Console.WriteLine(queue.Dequeue());
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+            Console.WriteLine(queue.Dequeue());
+            queue.Enqueue(7);
+            queue.Enqueue(8);
+
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+
+        }
+
+        private readonly Stack<T> S1 = new Stack<T>();
+        private readonly Stack<T> S2 = new Stack<T>();
+
+        public void Enqueue(T t)
+        {
+            S1.Push(t);
+        }
+
+        public T Dequeue()
+        {
+            if (S2.Count == 0)
+            {
+                if (S1.Count == 0)
+                {
+                    throw new Exception("Queue is empty");
+                }
+
+                while (S1.Count > 0)
+                    S2.Push(S1.Pop());
+            }
+
+            return S1.Pop();
+        }
+    }
+}

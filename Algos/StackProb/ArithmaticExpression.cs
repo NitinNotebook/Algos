77dd198a9d3a1924391d7418982cb3a5883b856a
Nitin Mahajan <nimaha@microsoft.com>
2020-11-26 21:52:52 +0530

    Expression conversion from infix to post fix, infix evaulation with brackets, Longest Rectange Histogram

--- Algos/StackProb/ArithmaticExpression.cs
+++ Algos/StackProb/ArithmaticExpression.cs
@@ -16,6 +16,12 @@ namespace Algos.StackProb
 
             UnitTest_PostFix("5 2 +", 7);
             UnitTest_PostFix("50 20 * 20 / 20 +", 70);
+            UnitTest_PostFix("50 20 15 2 * + +", 100);
+
+
+            UnitTest_ConvertToPostFix("A * B + C / D", "A B * C D / +");
+            UnitTest_ConvertToPostFix("A * ( B + C ) / D", "A B C + * D /");
+            UnitTest_ConvertToPostFix("A * ( B + C / D )", "A B C D / + *");
         }
 
         private static void UnitTest(string expr, int result)
@@ -24,10 +30,10 @@ namespace Algos.StackProb
             Console.WriteLine($"{expected == result}, {expr} = {expected}");
         }
 
-        private static bool IsOperator(char valOp)
+        private static bool IsOperator(string valOp)
         {
-            return valOp == '*' || valOp == '/' || valOp == '+' || valOp == '-';
-        }        
+            return valOp[0] == '*' || valOp[0] == '/' || valOp[0] == '+' || valOp[0] == '-';
+        }
 
         private static string ApplyOperator(string num1, string num2, string op)
         {
@@ -93,7 +99,7 @@ namespace Algos.StackProb
                     }
                     stack.Pop(); //flush the "(" from stack
                 }
-                else if (IsOperator(arrExpr[i][0]))
+                else if (IsOperator(arrExpr[i]))
                 {
                     while (opStack.Count > 0 && GetOpPrecedence(arrExpr[i]) < GetOpPrecedence(opStack.Peek()))
                     {
@@ -117,6 +123,52 @@ namespace Algos.StackProb
             return int.Parse(stack.Pop());
         }
 
+
+        private static void UnitTest_ConvertToPostFix(string expr, string expected)
+        {
+            var result = ConvertInfixToPostFix(expr);
+            Console.WriteLine($"{expected == result}, {expr}, {expected} = {result}");
+        }
+
+        public static string ConvertInfixToPostFix(string expr)
+        {
+            expr = $"( {expr} )";
+            var arrExpr = expr.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string postFix = string.Empty;
+            var stack = new Stack<string>();
+            for (int i = 0; i < arrExpr.Length; i++)
+            {
+                if (arrExpr[i] == "(")
+                {
+                    stack.Push("(");
+                }
+                else if (arrExpr[i] == ")")
+                {
+                    while (stack.Count > 0 && stack.Peek() != "(")
+                    {
+                        postFix = $"{postFix} {stack.Pop()}";
+                    }
+                    stack.Pop();
+                }
+                else if (IsOperator(arrExpr[i]))
+                {
+                    while (stack.Count > 0 && stack.Peek()  != "(" && GetOpPrecedence(stack.Peek()) > GetOpPrecedence(arrExpr[i]))
+                    {
+                        postFix = $"{postFix} {stack.Pop()}";
+                    }
+                    stack.Push(arrExpr[i]);
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(postFix))
+                        postFix = arrExpr[i];
+                    else
+                        postFix = $"{postFix} {arrExpr[i]}";
+                }
+            }
+            return postFix;
+        }
+
         private static void UnitTest_PostFix(string expr, int result)
         {
             int expected = EvaluatePostfix(expr);
@@ -129,7 +181,7 @@ namespace Algos.StackProb
             var stack = new Stack<string>();
             for (int i = 0; i < arrExpr.Length; i++)
             {
-                if (IsOperator(arrExpr[i][0]))
+                if (IsOperator(arrExpr[i]))
                 {
                     var prevNumber = ApplyOperator(stack.Pop(), stack.Pop(), arrExpr[i]);
                     stack.Push(prevNumber);
@@ -142,5 +194,6 @@ namespace Algos.StackProb
 
             return int.Parse(stack.Pop());
         }
+
     }
 }
---
+++ Algos/StackProb/LargestRectangeInHistogram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algos.StackProb
+{
+    public class LargestRectangeInHistogram
+    {
+        public static void Test()
+        {
+            UnitTest(new int[] { 2, 1, 5, 6, 2, 3 }, 10);
+            UnitTest(new int[] { 2, 1, 2 }, 3);
+        }
+
+        private static void UnitTest(int[] heights, int expected)
+        {
+            int result = LargestRectangleArea(heights);
+            Console.WriteLine($"{expected == result}");
+        }
+
+        public static int LargestRectangleArea(int[] heights)
+        {            
+            int maxHeight = heights[0];
+            Stack<int[]> stack = new Stack<int[]>();
+            for (int i = 0; i < heights.Length; i++)
+            {
+                int start = i;
+                while (stack.Count > 0 && stack.Peek()[1] > heights[i])
+                {
+                    var bar = stack.Pop();
+                    maxHeight = Math.Max(maxHeight, bar[1] * (i - bar[0]));
+                    start = bar[0];
+                }
+
+                stack.Push(new int[] { start, heights[i] });
+            }
+
+            while (stack.Count > 0)
+            {
+                var bar = stack.Pop();
+                maxHeight = Math.Max(maxHeight, bar[1] * (heights.Length - bar[0]));
+            }
+
+            return maxHeight;
+        }
+    }
+}

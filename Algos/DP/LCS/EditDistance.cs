d00f0f77a79df0e789b610e9b6ab64f62576cbc1
Nitin Mahajan <nimaha@microsoft.com>
2020-12-23 19:42:55 +0530

    LRS - Repeating and Edit Distance

---
+++ Algos/DP/LCS/EditDistance.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Algos.DP.LCS
+{
+    /// <summary>
+    /// Edit Distance - Levenshtein
+    /// Minimum number of single-character edits(i.e.insertions, deletions, or substitutions) required to change one word into the other
+    /// </summary>
+    public static class EditDistance
+    {
+        public static void Test()
+        {
+            Console.WriteLine("-----Edit Distance----");
+            UnitTest("baa", "a", 2);
+            UnitTest("nitin", "natan", 2);
+            UnitTest("abcd", "efgh", 4);
+            UnitTest("aabbccdd", "abbccdd", 1);
+        }
+
+        private static void UnitTest(string s1, string s2, int expected)
+        {
+            //var result = EditDistance_Rec(s1, s2, s1.Length - 1, s2.Length - 1);
+            var result = EditDistance_Iter(s1, s2);
+            Console.WriteLine($"{expected == result}, {expected}={result}");
+        }
+
+
+        private static int EditDistance_Iter(string s1, string s2)
+        {
+            int n1 = s1.Length + 1;
+            int n2 = s2.Length + 1;
+            var dp = new int[n1, n2];
+
+            for (int i = 0; i < n1; i++)
+            {
+                for (int j = 0; j < n2; j++)
+                {
+                    if (i == 0)
+                        dp[i, j] = j;
+                    else if (j == 0)
+                        dp[i, j] = i;
+                    else if (s1[i - 1] == s2[j - 1])
+                    {
+                        dp[i, j] = dp[i - 1, j - 1];
+                    }
+                    else
+                    {
+                        dp[i, j] = 1 + Math.Min(dp[i - 1, j], dp[i, j - 1]);
+                        dp[i, j] = Math.Min(dp[i, j], 1 + dp[i - 1, j - 1]);
+                    }
+                }
+            }
+
+            return dp[n1 - 1, n2 - 1];
+        }
+
+
+        private static int EditDistance_Rec(string s1, string s2, int i, int j)
+        {
+            if (i < 0)
+                return j + 1;
+
+            if (j < 0)
+                return i + 1;
+
+            if (s1[i] == s2[j])
+            {
+                return EditDistance_Rec(s1, s2, i - 1, j - 1);
+            }
+
+            int r1 = 1 + EditDistance_Rec(s1, s2, i - 1, j - 1);
+            int r2 = 1 + EditDistance_Rec(s1, s2, i - 1, j);
+            int r3 = 1 + EditDistance_Rec(s1, s2, i, j - 1);
+            return 1 + Math.Min(r1, Math.Min(r2, r3));
+        }
+    }
+}
---
+++ Algos/DP/LCS/LongestRepeatingSubsequence.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Algos.DP.LCS
+{
+    /// <summary>
+    /// Given a sequence, find the length of its longest repeating subsequence (LRS). 
+    /// A repeating subsequence will be the one that appears at least twice in the original sequence and is not overlapping
+    /// (i.e. none of the corresponding characters in the repeating subsequences have the same index).
+    /// </summary>
+    public static class LongestRepeatingSubsequence
+    {
+        public static void Test()
+        {
+            Console.WriteLine("------Longest Repeating Subsequence----");
+            UnitTest("baa", "a");
+            UnitTest("bbaabbaa", "bbaa");
+            UnitTest("baadxx", "ax");
+            UnitTest("abcd", "");
+            UnitTest("aaaa", "aaa");
+            UnitTest("aabdbcec", "abc");
+        }
+
+        private static void UnitTest(string s1, string expected)
+        {
+            var result = LRS_rec(s1, s1.Length - 1, s1.Length - 1);
+            //var result = LRS(s1);
+            Console.WriteLine($"{expected == result}, {expected}={result}");
+        }
+
+        public static string LRS(string s1)
+        {
+            int n = s1.Length + 1;
+            var dp = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == 0 || j == 0) continue;
+
+                    if (i != j && s1[i - 1] == s1[j - 1])
+                    {
+                        dp[i, j] = 1 + dp[i - 1, j - 1];
+                    }
+                    else
+                    {
+                        dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+                    }
+                }
+            }
+
+            int r = n - 1;
+            int c = n - 1;
+            string lrs = string.Empty;
+            while (r > 0 && c > 0)
+            {
+                if (r != c && s1[r - 1] == s1[c - 1])
+                {
+                    lrs = s1[r - 1] + lrs;
+                    r--;
+                    c--;
+                }
+                else
+                {
+                    if (dp[r - 1, c] > dp[r, c - 1])
+                    {
+                        r--;
+                    }
+                    else
+                    {
+                        c--;
+                    }
+                }
+            }
+            return lrs;
+        }
+
+
+        public static string LRS_rec(string s1, int i, int j)
+        {
+            if (i < 0 || j < 0) return "";
+
+            if (i != j && s1[i] == s1[j])
+            {
+                return LRS_rec(s1, i - 1, j - 1) + s1[i];
+            }
+
+            var a1 = LRS_rec(s1, i - 1, j);
+            var a2 = LRS_rec(s1, i, j - 1);
+
+            return a1.Length > a2.Length ? a1 : a2;
+
+        }
+    }
+}

5764844fca91105b592b53bbba565fdf778c7003
Nitin Mahajan <nimaha@microsoft.com>
2020-12-24 19:40:37 +0530

    Longest Palindromic Subsequence and Substring

---
+++ Algos/DP/LCS/LongestPalindromeSubsequence.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Algos.DP
+{
+    //Given a sequence, find the length of its Longest Palindromic Subsequence (LPS).
+    public class LongestPalindromeSubsequence
+    {
+        public static void Test()
+        {
+            Console.WriteLine("Longest Palindrome Subsequence");
+            UnitTest("banana", "anana");
+            UnitTest("nitin", "nitin");
+            UnitTest("ananab", "anana");
+            UnitTest("abcd", "a");
+            UnitTest("baaaa", "aaaa");
+            UnitTest("naitbin", "nitin");
+        }
+
+        public static void UnitTest(string s, string expected)
+        {
+            //string result = GetLPS(s);
+            //Console.WriteLine($"{expected == result}");
+            //var result = LPSCount_Rec(s, 0, s.Length - 1);
+            var result = LPSCount_BottomUp(s);
+            Console.WriteLine($"{expected.Length == result}, {expected.Length} = {result}");
+        }
+
+        /// <summary>
+        /// We can start processing from the beginning and the end of the sequence. So at any step, we have two options:
+        /// If element at the beginning and the end are same, we increment our count by two and make a recursive call for the remaining sequence.
+        /// Else We will skip the element either from the beginning or the end to make two recursive calls for the remaining subsequence.
+        /// </summary>
+        public static int LPSCount_Rec(string s1, int start, int end)
+        {
+            if (start == end) return 1;
+            if (start > end) return 0;
+
+            if (s1[start] == s1[end])
+                return 2 + LPSCount_Rec(s1, start + 1, end - 1);
+
+            return Math.Max(LPSCount_Rec(s1, start + 1, end), LPSCount_Rec(s1, start, end - 1));
+        }
+
+
+        public static int LPSCount_BottomUp(string s)
+        {
+            var dp = new int[s.Length, s.Length];
+
+            for (int j = 0; j < s.Length; j++)
+            {
+                for (int i = j; i >= 0; i--)
+                {
+                    if (i == j)
+                        dp[i, j] = 1;
+                    else if (s[i] == s[j])
+                        dp[i, j] = dp[i + 1, j - 1] + 2;
+                    else
+                        dp[i, j] = Math.Max(dp[i + 1, j], dp[i, j - 1]);
+                }
+            }
+            return dp[0, s.Length - 1];
+        }
+    }
+}
---
+++ Algos/DP/LCS/LongestPalindromeSubstring.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Algos.DP.LCS
+{
+    //Given a sequence, find the length of its Longest Palindromic Subsequence (LPS).
+    public class LongestPalindromeSubstring
+    {
+        public static void Test()
+        {
+            Console.WriteLine("Longest Palindromic Substring");
+            UnitTest("banana", "anana");
+            UnitTest("nitin", "nitin");
+            UnitTest("ananab", "anana");
+            UnitTest("abcd", "a");
+            UnitTest("baaaa", "aaaa");
+            UnitTest("aaa", "aaa");
+        }
+
+        public static void UnitTest(string s, string expected)
+        {
+            string result = GetLPS(s);
+            Console.WriteLine($"{expected == result}");
+            //var result = LPSCount_Rec(s, 0, s.Length - 1);
+            //var result = LPSLength_Rec(s, 0, s.Length - 1);
+            //Console.WriteLine($"{expected.Length == result}, {expected.Length} = {result}");
+        }
+
+        public static string GetLPS(string s)
+        {
+            var dp = new bool[s.Length, s.Length];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                dp[i, i] = true;
+            }
+
+            int maxRow = 0;
+            int maxCol = 0;
+
+            for (int col = 1; col < s.Length; col++)
+            {
+                for (int row = col - 1; row >= 0; row--)
+                {
+                    if (s[row] != s[col]) continue;
+
+                    dp[row, col] = col - row == 1 || dp[row + 1, col - 1];
+
+                    if (dp[row, col] && col - row > maxCol - maxRow)
+                    {
+                        maxRow = row;
+                        maxCol = col;
+                    }
+                }
+            }
+            return s.Substring(maxRow, maxCol + 1 - maxRow);
+        }
+
+        public static int LPSLength_Rec(string s1, int start, int end)
+        {
+            if (start == end) return 1;
+            if (start > end) return 0;
+
+            if (s1[start] == s1[end])
+            {
+                var r1 = LPSLength_Rec(s1, start + 1, end - 1);
+                if (r1 == end - start - 1) //length from recursive call will be without first and last char
+                    return 2 + r1;
+            }
+
+            return Math.Max(LPSLength_Rec(s1, start + 1, end), LPSLength_Rec(s1, start, end - 1));
+        }
+
+        /// Given a string, find the total number of palindromic substrings in it.
+        public static int LPSCount(string s1)
+        {
+            int count = 0;
+
+            var dp = new bool[s1.Length, s1.Length];
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                dp[i, i] = true;
+                count++;
+            }
+
+            for (int col = 1; col < s1.Length; col++)
+            {
+                for (int row = col - 1; row >= 0; row--)
+                {
+                    if (s1[row] != s1[col]) continue;
+
+                    //col-row = 1 --> two length string with same characters at both positions
+                    dp[row, col] = col - row == 1 || dp[row + 1, col - 1];
+
+                    if (dp[row, col]) count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
